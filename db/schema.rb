0e4e01c1e69f5d9ec55af8f2d1c1546524f9779c
Rohit <rohit@Rohits-MacBook-Air.local>
2015-06-22 21:10:55 +0530

    Initial commit

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150101155005) do
+ActiveRecord::Schema.define(version: 20141028001605) do
 
   create_table "visitors", force: :cascade do |t|
     t.string   "email"
