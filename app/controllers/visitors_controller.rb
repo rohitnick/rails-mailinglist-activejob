a79be506d7d01286bea4f2abc99ae749bdad638e
Rohit <rohit@Rohits-MacBook-Air.local>
2015-06-24 13:33:15 +0530

    adding ip address for visitor

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -10,6 +10,8 @@ class VisitorsController < ApplicationController
 
   def create
     @visitor = Visitor.new(visitor_params)
+    @visitor.ip_address = request.remote_ip
+    binding.pry
     if @visitor.save
       redirect_to root_path, notice: "Signed up #{@visitor.email}."
     else
